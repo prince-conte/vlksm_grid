47530a1dfda3c9aff3d7ccfeed104e25285379c2
alexanderkozlov <alexanderk@coalla.ru>
2017-08-30 19:46:45 +0300

    full

---
+++ app/assets/js/components/titleHeight.js
@@ -0,0 +1,25 @@
+
+$(function() {
+
+    $('.js-item-title').equalHeight({
+        // groupByTop: false,
+        // resizeTimeout: 20,
+        // updateOnDOMReady: true,
+        // updateOnDOMLoad: false
+    })
+
+    $('.js-item-title-big').equalHeight({
+        // groupByTop: false,
+        // resizeTimeout: 20,
+        // updateOnDOMReady: true,
+        // updateOnDOMLoad: false
+    })
+    $('.js-item-title-max').equalHeight({
+        // groupByTop: false,
+        // resizeTimeout: 20,
+        // updateOnDOMReady: true,
+        // updateOnDOMLoad: false
+    })
+
+
+});
